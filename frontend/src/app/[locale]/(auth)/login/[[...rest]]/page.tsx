f022b5a8a6fe876d7af22c1933a1c806cd2e05c2
Jean Rakotoarison <jd.rakotoarison@gmail.com>
2026-05-21 21:27:10 +0300

    link login page logo to homepage

--- frontend/src/app/[locale]/(auth)/login/[[...rest]]/page.tsx
+++ frontend/src/app/[locale]/(auth)/login/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const LoginPage: React.FC = () => {
 	return (
 		<div className="min-h-screen flex items-center justify-center p-4">
-			<SignIn />
+			<SignIn appearance={{ layout: { logoLinkUrl: "/" } }} />
 		</div>
 	);
 };
